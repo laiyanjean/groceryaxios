7557cfaf55baf29b225fcdde2fc5502c44b2562f
Yan Jean <laiyanjean@gmail.com>
2020-03-25 17:29:40 +0800

    set up firebase deployment

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "AIzaSyA3kbSiijPFP088YPywVhakCk7STDJxU2k",
-    authDomain: "grocery-project-29a02.firebaseapp.com",
-    databaseURL: "https://grocery-project-29a02.firebaseio.com",
-    projectId: "grocery-project-29a02",
-    storageBucket: "grocery-project-29a02.appspot.com",
-    messagingSenderId: "182143333394",
-    appId: "1:182143333394:web:1dfc5a9829830b56f814cb",
-    measurementId: "G-DME54WZH49"
+
   };
   
   firebase.initializeApp(firebaseConfig);
