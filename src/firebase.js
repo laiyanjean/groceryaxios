2b74654e19835e95a42ca389da9486db9e5390df
Yan Jean <laiyanjean@gmail.com>
2020-03-25 18:13:44 +0800

    added project ID

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,9 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-
+    authDomain: "grocery-axios.firebaseapp.com",
+    databaseURL: "https://grocery-axios.firebaseio.com",
+    projectId: "grocery-axios",
   };
   
   firebase.initializeApp(firebaseConfig);
